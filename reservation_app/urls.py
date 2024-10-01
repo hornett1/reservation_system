fd626bccc7dbf900d4d21eb728c48133f452b7f6
Matthew <matt2010k@gmail.com>
2024-10-01 19:35:54 +0300

    -

--- reservation_app/urls.py
+++ reservation_app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('users/', user_list, name="users-list")
+    path('users/', user_list, name="users-list"),
+    path('locations/', location_list, name="locations-list")
 ]
--- reservation_app/views.py
+++ reservation_app/views.py
@@ -3,4 +3,8 @@ from .models import *
 
 def user_list(request):
     users = User.objects.all()
-    return render(request, 'reservation_app/index.html', {'books': users})
+    return render(request, 'reservation_app/index.html', {'users': users})
+
+def location_list(request):
+    locations = Location.objects.all()
+    return render(request, 'reservation_app/index.html', {'locations': locations})
